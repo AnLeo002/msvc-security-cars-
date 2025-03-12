9603fa92c1d317693d8a19366d5f27a41e1b3d43
Andre <lulesaaso2@gmail.com>
2025-03-12 18:08:18 -0500

    create-IKeycloakService-service

---
+++ src/main/java/com/security/sevice/IKeycloakService.java
@@ -0,0 +1,14 @@
+package com.security.sevice;
+
+import com.security.controller.dto.UserDTO;
+import org.keycloak.representations.idm.UserRepresentation;
+
+import java.util.List;
+
+public interface IKeycloakService {
+    List<UserRepresentation> findAllUsers();
+    List<UserRepresentation> searchUserByUsername();
+    String createUser(UserDTO userDTO);
+    void deleteUser(String id);
+    void updateUser(String id,UserDTO userDTO);
+}
