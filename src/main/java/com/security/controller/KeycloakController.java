b5fd40c09b556a002b072916cb0b996bb233cfdc
Andre <lulesaaso2@gmail.com>
2025-03-12 19:59:58 -0500

    create-KeycloakController-controller

---
+++ src/main/java/com/security/controller/KeycloakController.java
@@ -0,0 +1,46 @@
+package com.security.controller;
+
+import com.security.controller.dto.UserDTO;
+import com.security.sevice.IKeycloakService;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.Path;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/keycloak/user")
+public class KeycloakController {
+    @Autowired
+    private IKeycloakService keycloakService;
+    @GetMapping("/search")
+    @PreAuthorize("hasRole('admin_client_role')")
+    public ResponseEntity<List<UserRepresentation>> findAllUsers(){
+        return ResponseEntity.ok(keycloakService.findAllUsers());
+    }
+    @GetMapping("/search/{username}")
+    public ResponseEntity<List<UserRepresentation>> findUserByUsername(@PathVariable String username){
+        return ResponseEntity.ok(keycloakService.searchUserByUsername(username));
+    }
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody UserDTO userDTO) throws URISyntaxException {
+        String response = keycloakService.createUser(userDTO);
+        return ResponseEntity.created(new URI("/keycloak/user/create")).body(response);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateUser(@RequestBody UserDTO userDTO, @PathVariable String id) throws URISyntaxException {
+        keycloakService.updateUser(id,userDTO);
+        return ResponseEntity.ok("User updated successfully");
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable String id){
+        keycloakService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
